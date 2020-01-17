4a536ff9642b03e1025ab714bc9930c02335cbcc
jovan-trkulja-drugi <jovantrkulja9@hotmail.com>
2020-01-17 17:05:54 +0100

    JPA 17:05 17.01 dodat equals & hashCode

--- AutoServisJPA/src/model/Popravka.java
+++ AutoServisJPA/src/model/Popravka.java
@@ -169,4 +169,76 @@ public class Popravka implements Serializable {
 		return cena;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cena == null) ? 0 : cena.hashCode());
+		result = prime * result + ((datumPrijema == null) ? 0 : datumPrijema.hashCode());
+		result = prime * result + ((datumZavrsetka == null) ? 0 : datumZavrsetka.hashCode());
+		result = prime * result + idPopravka;
+		result = prime * result + ((opisPopravke == null) ? 0 : opisPopravke.hashCode());
+		result = prime * result + ((radniks == null) ? 0 : radniks.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((uslugas == null) ? 0 : uslugas.hashCode());
+		result = prime * result + ((vozilos == null) ? 0 : vozilos.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Popravka other = (Popravka) obj;
+		if (cena == null) {
+			if (other.cena != null)
+				return false;
+		} else if (!cena.equals(other.cena))
+			return false;
+		if (datumPrijema == null) {
+			if (other.datumPrijema != null)
+				return false;
+		} else if (!datumPrijema.equals(other.datumPrijema))
+			return false;
+		if (datumZavrsetka == null) {
+			if (other.datumZavrsetka != null)
+				return false;
+		} else if (!datumZavrsetka.equals(other.datumZavrsetka))
+			return false;
+		if (idPopravka != other.idPopravka)
+			return false;
+		if (opisPopravke == null) {
+			if (other.opisPopravke != null)
+				return false;
+		} else if (!opisPopravke.equals(other.opisPopravke))
+			return false;
+		if (radniks == null) {
+			if (other.radniks != null)
+				return false;
+		} else if (!radniks.equals(other.radniks))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (uslugas == null) {
+			if (other.uslugas != null)
+				return false;
+		} else if (!uslugas.equals(other.uslugas))
+			return false;
+		if (vozilos == null) {
+			if (other.vozilos != null)
+				return false;
+		} else if (!vozilos.equals(other.vozilos))
+			return false;
+		return true;
+	}
+	
+	
+
 }
--- AutoServisJPA/src/model/Status.java
+++ AutoServisJPA/src/model/Status.java
@@ -69,4 +69,40 @@ public class Status implements Serializable {
 		return popravka;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idStatus;
+		result = prime * result + ((opis == null) ? 0 : opis.hashCode());
+		result = prime * result + ((popravkas == null) ? 0 : popravkas.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Status other = (Status) obj;
+		if (idStatus != other.idStatus)
+			return false;
+		if (opis == null) {
+			if (other.opis != null)
+				return false;
+		} else if (!opis.equals(other.opis))
+			return false;
+		if (popravkas == null) {
+			if (other.popravkas != null)
+				return false;
+		} else if (!popravkas.equals(other.popravkas))
+			return false;
+		return true;
+	}
+	
+	
+
 }
